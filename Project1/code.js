24b71c55423ce590c20a9f65ae15ef40e538ede2
M7md Odeh <1200089@student.birzeit.edu>
2023-06-16 20:43:54 +0300

    Update code.js

--- Project1/code.js
+++ Project1/code.js
@@ -316,6 +316,166 @@ function OneOrTwoPlayers(){
   generateBtn2.addEventListener("click", chooseBlackOrWhite);
 }
 
+// function to minimax algorithm to find the best move
+function minimax(newBoard, player){
+  //available spots
+  var availSpots = emptySquares(newBoard);
+
+  // checks for the terminal states such as win, lose, and tie and returning a value accordingly
+  if (checkWin(newBoard, player)){
+      return {score:-10};
+  }
+  else if (checkWin(newBoard, aiPlayer)){
+      return {score:10};
+  }
+  else if (availSpots.length === 0){
+      return {score:0};
+  }
+
+  // an array to collect all the objects
+  var moves = [];
+
+  // loop through available spots
+  for (var i = 0; i < availSpots.length; i++){
+      //create an object for each and store the index of that spot that was stored as a number in the object's index key
+      var move = {};
+      move.index = newBoard[availSpots[i]];
+
+      // set the empty spot to the current player
+      newBoard[availSpots[i]] = player;
+
+      //if collect the score resulted from calling minimax on the opponent of the current player
+      if (player == aiPlayer){
+          var result = minimax(newBoard, huPlayer);
+          move.score = result.score;
+      }
+      else{
+          var result = minimax(newBoard, aiPlayer);
+          move.score = result.score;
+      }
+
+      //reset the spot to empty
+      newBoard[availSpots[i]] = move.index;
+
+      // push the object to the array
+      moves.push(move);
+  }
+
+  // if it is the computer's turn loop over the moves and choose the move with the highest score
+  var bestMove;
+  if(player === aiPlayer){
+      var bestScore = -10000;
+      for(var i = 0; i < moves.length; i++){
+          if(moves[i].score > bestScore){
+              bestScore = moves[i].score;
+              bestMove = i;
+          }
+      }
+  }else{
+
+  // else loop over the moves and choose the move with the lowest score
+      var bestScore = 10000;
+      for(var i = 0; i < moves.length; i++){
+          if(moves[i].score < bestScore){
+              bestScore = moves[i].score;
+              bestMove = i;
+          }
+      }
+  }
+
+  // return the chosen move (object) from the array to the higher depth
+  return moves[bestMove];
+}
+
+// function for alpha beta pruning algorithm to find the best move
+function alphabeta(newBoard, player, alpha, beta){
+  //available spots
+  var availSpots = emptySquares(newBoard);
+
+  // checks for the terminal states such as win, lose, and tie and returning a value accordingly
+  if (checkWin(newBoard, player)){
+      return {score:-10};
+  }
+  else if (checkWin(newBoard, aiPlayer)){
+      return {score:10};
+  }
+  else if (availSpots.length === 0){
+      return {score:0};
+  }
+
+  // an array to collect all the objects
+  var moves = [];
+
+  // loop through available spots
+  for (var i = 0; i < availSpots.length; i++){
+      //create an object for each and store the index of that spot that was stored as a number in the object's index key
+      var move = {};
+      move.index = newBoard[availSpots[i]];
+      
+      // set the empty spot to the current player
+      newBoard[availSpots[i]] = player;
+
+      //if collect the score resulted from calling alphabeta on the opponent of the current player
+      if (player == aiPlayer){
+
+          var result = alphabeta(newBoard, huPlayer, alpha, beta);
+          move.score = result.score;
+          if(move.score > alpha){
+            alpha = move.score;
+          }
+      }
+      else{
+          var result = alphabeta(newBoard, aiPlayer, alpha, beta);
+          move.score = result.score;
+          if(move.score < beta){
+            beta = move.score;
+          }
+      }
+      
+      //reset the spot to empty
+      newBoard[availSpots[i]] = move.index;
+
+      // push the object to the array
+      moves.push(move);
+
+      if(alpha >= beta){
+        break;
+      }
+  }
+
+  // if it is the computer's turn loop over the moves and choose the move with the highest score
+  var bestMove;
+  if(player === aiPlayer){
+      var bestScore = -10000;
+      for(var i = 0; i < moves.length; i++){
+          if(moves[i].score > bestScore){
+              bestScore = moves[i].score;
+              bestMove = i;
+          }
+      }
+  }else{
+    
+  // else loop over the moves and choose the move with the lowest score
+      var bestScore = 10000;
+      for(var i = 0; i < moves.length; i++){
+          if(moves[i].score < bestScore){
+              bestScore = moves[i].score;
+              bestMove = i;
+          }
+      }
+  }
+
+  // return the chosen move (object) from the array to the higher depth
+  return moves[bestMove];
+}
+
+
+
+
+
+
+
+
 
 let generateBtn = document.getElementById("PlayGame");
-generateBtn.addEventListener("click", OneOrTwoPlayers);
+generateBtn.addEventListener("click", OneOrTwoPlayers);z
